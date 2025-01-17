c2a46c4fb4f5ffc98c9300c80fd2f79b5a27df39
Felipe <2829082+synchronizing@users.noreply.github.com>
2025-01-17 18:57:19 -0500

    Updated versioning.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="qbase58",
-    version="1.0.1",
+    version="1.0.2",
     author="Quick Vectors",
     author_email="felipe@qvecs.com",
     description="Quick Base58 encoding/decoding.",
